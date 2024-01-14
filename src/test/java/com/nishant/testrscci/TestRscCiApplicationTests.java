9e4b16b45b9981af950dcffac8a1c0b05da867c8
Nishant Ingle <nishant.ingle@stud-mail.uni-wuerzburg.de>
2024-01-14 16:47:05 +0100

    Create test endpoint

---
+++ src/test/java/com/nishant/testrscci/TestRscCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishant.testrscci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRscCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
